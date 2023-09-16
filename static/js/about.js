4a75fdd3d62d0621e129b79702b043f5e5a81dab
nano <kaweepatinn1@gmail.com>
2023-09-16 17:27:20 +0800

    stuff and increase shadow opacity in grid

---
+++ static/js/about.js
@@ -0,0 +1,10 @@
+document.body.onscroll = () => {
+    //calculate the current scroll progress as a percentage
+    scrollPercent =
+        ((document.documentElement.scrollTop || document.body.scrollTop) /
+            ((document.documentElement.scrollHeight ||
+                document.body.scrollHeight) -
+                document.documentElement.clientHeight)) *
+        100;
+    // console.log(scrollPercent);
+}
